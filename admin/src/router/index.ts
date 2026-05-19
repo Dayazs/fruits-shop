12e7e03a2c7ad3daeff9d849374695b541dffee0
Dayazs <Dayazs@outlook.com>
2026-05-19 21:03:22 +0800

    feat: 新增路由，并对路由权限进行检查

--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -2,28 +2,35 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    component: () => import('@/layout/index.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        component: () => import('@/views/Home.vue'),
-        meta: { title: '首页' },
-      },
-    ],
-  },
-  {
     path: '/login',
     name: 'Login',
     component: () => import('@/views/Login.vue'),
     meta: { title: '登录' },
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: () => import('@/views/Register.vue'),
-    meta: { title: '注册' },
+    path: '/',
+    component: () => import('@/layout/index.vue'),
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/Dashboard.vue'),
+        meta: { title: '首页', requiresAuth: true, permission: 'dashboard' },
+      },
+      {
+        path: 'goods',
+        name: 'Goods',
+        component: () => import('@/views/goods/index.vue'),
+        meta: { title: '商品页', requiresAuth: true, permission: 'goods' },
+      },
+      {
+        path: 'admin',
+        name: 'Admin',
+        component: () => import('@/views/admin/index.vue'),
+        meta: { title: '管理员页', requiresAuth: true, permission: 'admin' },
+      },
+    ],
   },
 ]
 
@@ -36,6 +43,56 @@ router.beforeEach((to, _from) => {
   if (to.meta.title) {
     document.title = to.meta.title as string
   }
+})
+
+router.beforeEach((to, from) => {
+  const token = localStorage.getItem('token')
+
+  // 访问登录页直接放行
+  if (to.path === '/login') {
+    if (token) {
+      return '/'
+    } else {
+      return true
+    }
+  }
+
+  // 需要登录的页面
+  if (!token) {
+    return '/login'
+  }
+
+  // 对于已登录的用户访问除登录页时，进行权限检查
+  const requiredPermission = to.meta?.permission
+  if (requiredPermission === 'dashboard') {
+    return true
+  }
+
+  try {
+    const permissionsStr = localStorage.getItem('permissions')
+
+    // 没有权限或者为空数组先放行
+    if (!permissionsStr || permissionsStr === '[]') {
+      return true
+    }
+
+    const permission = JSON.parse(permissionsStr)
+
+    // 超级管理员有所有权限
+    if (Array.isArray(permission) && permission.includes('*')) {
+      return true
+    }
+
+    // 检查路由权限
+    if (
+      !requiredPermission ||
+      (Array.isArray(permission) && permission.includes(requiredPermission))
+    ) {
+      return true
+    } else {
+      return '/dashboard'
+    }
+  } catch (err) {}
 })
 
 export default router
