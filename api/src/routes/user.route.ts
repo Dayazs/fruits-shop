86f6808241ab287cd9731074d7bcecd991a6cefd
Dayazs <Dayazs@outlook.com>
2026-05-19 15:13:08 +0800

    feat: 新增用户路由

---
+++ api/src/routes/user.route.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import {
+  getUserInfo,
+  login,
+  logout,
+  register,
+} from '../controllers/user.controller'
+
+const router = Router()
+
+// 测试接口
+router.get('/getUser', getUserInfo)
+
+router.post('/login', login) // 用户登录
+router.post('/register', register) // 用户注册
+router.post('/logout', logout) // 用户登出
+export default router
