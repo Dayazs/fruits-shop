fc0effe3753946dc9637fd8a15b5645e959d5267
Dayazs <Dayazs@outlook.com>
2026-05-19 21:05:53 +0800

    feat: 新增路由导入

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import userRouter from './user.route'
+import adminRouter from './admin.route'
+import goodsRouter from './goods.route'
 
 const router = Router()
 
-router.use('/user', userRouter)
-
+router.use('/user', userRouter) // 用户相关接口
+router.use('/admin', adminRouter) // 管理员相关接口
+router.use('/goods', goodsRouter) // 商品相关接口
 export default router
