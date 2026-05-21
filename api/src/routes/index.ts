434e743238214051da520453244b8ba20dfd22ef
Dayazs <Dayazs@outlook.com>
2026-05-21 19:35:11 +0800

    feat: 新增轮播图相关接口

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import userRouter from './user.route'
 import adminRouter from './admin.route'
 import goodsRouter from './goods.route'
+import bannerRouter from './banner.route'
 
 const router = Router()
 
 router.use('/user', userRouter) // 用户相关接口
 router.use('/admin', adminRouter) // 管理员相关接口
 router.use('/goods', goodsRouter) // 商品相关接口
+router.use('/banners', bannerRouter) // 轮播图相关接口
 export default router
