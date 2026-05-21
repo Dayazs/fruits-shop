b89d03aacd5a546d3727c7ebc04323bc9c7e0d0e
Dayazs <Dayazs@outlook.com>
2026-05-21 09:33:18 +0800

    feat: 购物车页面

---
+++ uniapp/pages/cart/cart.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="page">
+    <text class="title">购物车</text>
+    <text class="subtitle">暂无商品</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'CartPage'
+}
+</script>
+
+<style>
+.page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+.title {
+  font-size: 40rpx;
+  font-weight: bold;
+  color: #333;
+}
+.subtitle {
+  font-size: 28rpx;
+  color: #999;
+  margin-top: 16rpx;
+}
+</style>
