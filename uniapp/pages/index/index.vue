652f191980e320ade00ff6bd5ea914049969d9a9
Dayazs <Dayazs@outlook.com>
2026-05-22 19:30:15 +0800

    feat: 新增点击商品跳转指定商品详细页功能

--- uniapp/pages/index/index.vue
+++ uniapp/pages/index/index.vue
@@ -118,10 +118,7 @@
 	}
 
 	const handleGoodsTap = (goods) => {
-		uni.showToast({
-			title: goods.name,
-			icon: 'none'
-		})
+		uni.navigateTo({ url: `/pages/goods-detail/goods-detail?id=${goods.id}` })
 	}
 </script>
 
