8da1ce7db3b49dc3131a30167d80ee4342bd0ecb
Dayazs <Dayazs@outlook.com>
2026-05-22 09:32:07 +0800

    feat: 导入home路由

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,6 +3,7 @@ import userRouter from './user.route'
 import adminRouter from './admin.route'
 import goodsRouter from './goods.route'
 import bannerRouter from './banner.route'
+import homeRouter from './home.route'
 
 const router = Router()
 
@@ -10,4 +11,5 @@ router.use('/user', userRouter) // 用户相关接口
 router.use('/admin', adminRouter) // 管理员相关接口
 router.use('/goods', goodsRouter) // 商品相关接口
 router.use('/banners', bannerRouter) // 轮播图相关接口
+router.use('/home', homeRouter) // C 端首页接口（无需认证）
 export default router
