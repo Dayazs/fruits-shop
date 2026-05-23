549302407b335c137306cba7a6cd824ff7141177
Dayazs <Dayazs@outlook.com>
2026-05-23 11:33:07 +0800

    feat: 新增订单路由导入

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -4,6 +4,7 @@ import adminRouter from './admin.route'
 import goodsRouter from './goods.route'
 import bannerRouter from './banner.route'
 import homeRouter from './home.route'
+import orderRouter from './order.route'
 
 const router = Router()
 
@@ -12,4 +13,5 @@ router.use('/admin', adminRouter) // 管理员相关接口
 router.use('/goods', goodsRouter) // 商品相关接口
 router.use('/banners', bannerRouter) // 轮播图相关接口
 router.use('/home', homeRouter) // C 端首页接口（无需认证）
+router.use('/order', orderRouter) // 订单相关接口
 export default router
