492cbbab6bebf87aa0b4ab9cfc0006ad274ffa33
Dayazs <Dayazs@outlook.com>
2026-05-24 19:17:51 +0800

    feat: 新增仪表盘路由导入

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -5,6 +5,7 @@ import goodsRouter from './goods.route'
 import bannerRouter from './banner.route'
 import homeRouter from './home.route'
 import orderRouter from './order.route'
+import dashboardRouter from './dashboard.route'
 
 const router = Router()
 
@@ -14,4 +15,5 @@ router.use('/goods', goodsRouter) // 商品相关接口
 router.use('/banners', bannerRouter) // 轮播图相关接口
 router.use('/home', homeRouter) // C 端首页接口（无需认证）
 router.use('/order', orderRouter) // 订单相关接口
+router.use('/dashboard', dashboardRouter) // 仪表盘统计
 export default router
