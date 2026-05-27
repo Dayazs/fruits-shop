5818fb6ef1270385b1775262761f0c5ecb09891b
Dayazs <Dayazs@outlook.com>
2026-05-27 11:04:23 +0800

    feat: 新增element plus中文包

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -8,13 +8,14 @@ import './style/global.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(ElementPlus)
+app.use(ElementPlus, { locale: zhCn })
 app.use(createPinia())
 app.use(router)
 
